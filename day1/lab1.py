0fe5c04934404dee19eebd0e24ced5962eda9825
warddalston <ward.dalston@gmail.com>
2014-08-04 16:32:12 -0500

    Words document
    added the words document.

--- day1/lab1.py
+++ day1/lab1.py
@@ -2,68 +2,64 @@ def binarify(num):
   """convert positive integer to base 2"""
   if num<=0: return '0'
   digits = []
-  powers = [2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4 ,2 ,1]
-  for i in powers:
-  	digits.append(str(num/ i)) #append only works on strings?  
-  	num %= i 	
+  while num:
+  	digits.append(str(num % 2)) #append only works on strings?  
+  	num /= 2 	
   return ''.join(digits)
 
-#binarify(42)
+print binarify(42)
 
 
-def int_to_base(num, base):
-  """convert positive integer to a string in any base"""
-  if num<=0:  return '0' 
-  digits = []
-  for i in range(13, -1, -1):
-  	power = base**i
-  	digits.append(str(num/power))
-  	num %= power
-  return ''.join(digits)
-  
-#print int_to_base(48,4)
-
-  
-
-def base_to_int(string, base):
-  """take a string-formatted number and its base and return the base-10 integer"""
-  if string=="0" or base <= 0 : return 0 
-  power = range(len(string)-1,-1,-1)
-  result = 0 
-  for i in range(0, len(string)):
-  	result += int(string[i])*(base**power[i])
-  return result 
-  
-#print base_to_int("00000000030",4)
-
-def flexibase_add(str1, str2, base1, base2, base_out):
-  """add two numbers of different bases and return the sum"""
-  out = base_to_int(str1, base1) + base_to_int(str2, base2)
-  return  int_to_base(out, base_out)
-
-#print flexibase_add("000101", "0030", 2, 4, baseOUT=10)
-
-
-def flexibase_multiply(str1, str2, base1, base2, base_out):
-  """multiply two numbers of different bases and return the product"""
-  out = base_to_int(str1, base1) * base_to_int(str2, base2)
-  return  int_to_base(out, base_out)
-
-def romanify(num):
-  """given an integer, return the Roman numeral version"""
-  result = []
-  first=['','I','II','III','IV','V','VI','VII','VIII','IX']
-  second=['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
-  third=['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM']
-  fourth=['','M','MM','MMM']
-  result.append(fourth[num / 1000])
-  num %= 1000
-  result.append(third[num / 100])
-  num %= 100
-  result.append(second[num / 10])
-  num %= 10
-  result.append(first[num])
-  return ''.join(result)
-  
-def counter(number)
-	
+# def int_to_base(num, base):
+#   """convert positive integer to a string in any base"""
+#   if num<=0:  return '0' 
+#   digits = []
+#   for i in range(13, -1, -1):
+#   	power = base**i
+#   	digits.append(str(num/power))
+#   	num %= power
+#   return ''.join(digits)
+#   
+# #print int_to_base(48,4)
+# 
+#   
+# 
+# def base_to_int(string, base):
+#   """take a string-formatted number and its base and return the base-10 integer"""
+#   if string=="0" or base <= 0 : return 0 
+#   power = range(len(string)-1,-1,-1)
+#   result = 0 
+#   for i in range(0, len(string)):
+#   	result += int(string[i])*(base**power[i])
+#   return result 
+#   
+# #print base_to_int("00000000030",4)
+# 
+# def flexibase_add(str1, str2, base1, base2, base_out):
+#   """add two numbers of different bases and return the sum"""
+#   out = base_to_int(str1, base1) + base_to_int(str2, base2)
+#   return  int_to_base(out, base_out)
+# 
+# #print flexibase_add("000101", "0030", 2, 4, baseOUT=10)
+# 
+# 
+# def flexibase_multiply(str1, str2, base1, base2, base_out):
+#   """multiply two numbers of different bases and return the product"""
+#   out = base_to_int(str1, base1) * base_to_int(str2, base2)
+#   return  int_to_base(out, base_out)
+# 
+# def romanify(num):
+#   """given an integer, return the Roman numeral version"""
+#   result = []
+#   first=['','I','II','III','IV','V','VI','VII','VIII','IX']
+#   second=['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
+#   third=['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM']
+#   fourth=['','M','MM','MMM']
+#   result.append(fourth[num / 1000])
+#   num %= 1000
+#   result.append(third[num / 100])
+#   num %= 100
+#   result.append(second[num / 10])
+#   num %= 10
+#   result.append(first[num])
+#   return ''.join(result)
---
+++ day1/words.py
@@ -0,0 +1,44 @@
+# file = open('words.txt')
+# 
+# for line in file:
+#   word = line.strip()
+#   print word
+#   
+#   
+
+#does this word have an e?
+# def has_no_e(word):
+# 	return not("e" in word)
+# 
+# print has_no_e("cheese")
+
+#is a word made of only some letters 
+# def uses_only(word, available):
+# 	available = available.lower()
+# 	word = word.lower()
+# 	for letter in word:
+# 		if not(letter in available):
+#  			return False
+# 	return True 
+# 
+# print uses_only("dad", "da")
+
+# Uses ALL letters
+# def uses_only(word, letters):
+# 	letters = letters.lower()
+# 	word = word.lower()
+# 	for i in range(0,len(letters)):
+# 		if letters[i] in word:
+# 			continue
+# 		else:
+# 			return False
+# 	return True 
+# 	
+# print uses_only("Bagelelelelelele", "agleb")
+
+#in alphabetical order
+def is_abecedarian(word):
+	return "".join(sorted(word))==word
+	
+	
+print is_abecedarian("bagel")
