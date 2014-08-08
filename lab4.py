3f2ab55d1a064e87d5edecb721d60ad1d1895df2
warddalston <ward.dalston@gmail.com>
2014-08-08 13:40:02 -0500

    Lab 4 last update.

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from math import sqrt
+# from math import sqrt
 
 # In its simplest form, Euclid's algorithm starts with a pair of positive integers, and forms a new pair that consists of the smaller number and the difference between the larger and smaller numbers. The process repeats until the numbers in the pair are equal. That number then is the greatest common divisor of the original pair of integers.
 
@@ -23,17 +23,17 @@ from math import sqrt
 #
 # 	When the algorithm terminates, all the numbers in the list that are not marked are prime.
 # 
-
-def Sieve_Erasthones(number, nonprimes = [1], p = 2):
-	if p not in nonprimes:
-		for element in range( p + 1, number + 1):
-			if ( element % p == 0 and element not in nonprimes):
-				nonprimes.append(element)
-	if p >= sqrt(number):
-		return [element for element in range(2, number + 1) if element not in nonprimes]
-	return Sieve_Erasthones(number = number, nonprimes = nonprimes, p = (p + 1) )
-
-print Sieve_Erasthones(121)
+# 
+# def Sieve_Erasthones(number, nonprimes = [1], p = 2):
+# 	if p not in nonprimes:
+# 		for element in range( p + 1, number + 1):
+# 			if ( element % p == 0 and element not in nonprimes):
+# 				nonprimes.append(element)
+# 	if p >= sqrt(number):
+# 		return [element for element in range(2, number + 1) if element not in nonprimes]
+# 	return Sieve_Erasthones(number = number, nonprimes = nonprimes, p = (p + 1) )
+# 
+# print Sieve_Erasthones(121)
 
 #### towers of hanoi
 # empty tower, available, goal tower
@@ -42,11 +42,29 @@ print Sieve_Erasthones(121)
 # 3 game: play 2 game. Move 3 to goal tower.  Play 2 game.  WIN.
 # 4 game: play 3 game.  Move 4 to goal tower.  Play 3 game.  WIN.
 
-def Towers_of_Hanoi(start = [], goal = [], available = []):
-	if start == [] and (goal == [] or available == []):
-		return "Congrats, you win"
-	if 
-	start.(0,goal.pop(0))
+def Towers_of_Hanoi(start=None, goal=None, available=None):
+	if start == None:
+		start = []
+	if goal == None:
+		goal = []
+	if available == None:
+		available = []
+	goal.insert(0, start.pop(0))
+	print "goal tower:" + str(goal)
+	print "start tower:" + str(start)
+	print "available tower:" + str(available) + "\n\n"
+	if start == []:
+		if (goal==[] or available==[]):
+			print goal
+			return "Congratulations! You won the game! You're free!"
+		return Towers_of_Hanoi(start=available, goal=goal, available=start)
+	if start != []:
+		return Towers_of_Hanoi(start=start, goal=available, available=goal)	
+	
+#print Towers_of_Hanoi(start=[1])
+#print Towers_of_Hanoi(start=[1, 2])
+print Towers_of_Hanoi(start=[1, 2, 3])
+# print Towers_of_Hanoi(start=[1, 2, 3, 4])
 	
 	
 	 
