6534b79def527186b3b24165a47dcc8de7ef0c74
warddalston <ward.dalston@gmail.com>
2014-08-11 11:22:46 -0500

    hw2 midway
    about halfway done with the homework.

--- hw2/hw2_WARD_test.py
+++ hw2/hw2_WARD_test.py
@@ -1,39 +1,94 @@
 import unittest 
 
-from portfolio import Portfolio
+from portfolio import *
 
 class PortfolioTest(unittest.TestCase):
+
 	#create a portfolio to use in the tests
 	def setUp(self):
-		self.portfolio = Portfolio("Dalston Ward")
-#		self.HFH = Stock(20, "HFH")
-# 		self.DHW = Stock(15, "DHW")
-# 		self.BRT = MutualFund("BRT")
-# 		self.GHT = MutualFund("GHT")
+		self.portfolio = Portfolio(account_holder = "Dalston Ward", initial_deposit = 0)
+		self.HFH = Stock(20, "HFH")
+ 		self.DHW = Stock(15, "DHW")
+		self.BRT = MutualFund("BRT")
+		self.GHT = MutualFund("GHT")
+
+	#test to make sure the object is created correctly
+	def test_createPortfolio(self):
+		self.assertEqual(0, self.portfolio.funds)
+		self.assertEqual("Dalston Ward", self.portfolio.account_holder)
 
 	#test to add cash to the portfolio and make sure it prints the right amount.  
-	def addCashTest(self):
+	def test_addCash(self):
 		self.portfolio.addCash(300.50)
 		self.assertEqual(300.50, self.portfolio.funds)
+		
+		#test to remove cash to the portfolio and make sure it prints the right amount.  
+	def test_withdrawCash(self):
+		self.portfolio.addCash(300.50)
+		self.portfolio.withdrawCash(50)
+		self.assertEqual(250.50, self.portfolio.funds)
 	
-# 	#test to make sure that the stock created has the right name and price. 
-# 	def createStockTest(self):
-# 		self.assertEqual(20, self.HFH.price)
-# 		self.assertEqual("HFH", self.HFH.tickSym)
+	#test to make sure that the stock created has the right name and price. 
+	def test_createStock(self):
+		self.assertEqual(20, self.HFH.price)
+		self.assertEqual("HFH", self.HFH.tick_sym)
 # 	
-# 	#Test buying a stock and making sure that funds are removed from the account.  
-# 	def buyStockTest(self):
-# 		self.portfolio.buyStock(5, self.HFH)
-# 		self.assertEqual({"HFH" : 5}, self.portfolio.stocks)
-# 		self.assertEqual(200.50, self.portfolio.funds)
-# 	
-# 	def buyTwoStocks(self):
-# 		self.portfolio.buyStock(5, self.HFH)
-# 		self.
-# 			
-# 	def createMutualFundTest(self):
-# 		self.assertEqual("BRT", self.BRT.tickSym)
-# 		self.assertEqual("GHT", self.GHT.tickSym)
+	#Test buying a stock and making sure that funds are removed from the account.  
+	def test_buyStock(self):
+		self.portfolio.addCash(300.50) 
+		self.portfolio.buyStock(5, self.HFH)
+		self.assertEqual({"HFH" : 5}, self.portfolio.stocks)
+		self.assertEqual(200.50, self.portfolio.funds)
+	
+	#test buying two different stocks at once 	 	
+	def test_buyTwoStocks(self):
+		self.portfolio.addCash(300.50) 
+		self.portfolio.buyStock(5, self.HFH)
+		self.portfolio.buyStock(2, self.DHW)
+		self.assertEqual({"HFH" : 5, "DHW" : 2}, self.portfolio.stocks)
+		self.assertEqual(170.50, self.portfolio.funds)
+	
+	#test buying something you can't afford
+	def test_cantAffordThis(self):
+		self.portfolio.addCash(300.50) 
+		self.portfolio.buyStock(25, self.HFH)
+		self.assertEqual({}, self.portfolio.stocks)
+				
+	#test selling a stock.  Does portfolio change? 
+	def test_sellStocks(self):
+		self.portfolio.addCash(300.50) 
+		self.portfolio.buyStock(5, self.HFH)
+		self.portfolio.sellStock(3, self.HFH)
+		self.assertEqual({"HFH" : 2}, self.portfolio.stocks)
+	
+	#test selling all of your stock.  
+	def test_sellallStocks(self):
+		self.portfolio.addCash(300.50) 
+		self.portfolio.buyStock(5, self.HFH)
+		self.portfolio.sellStock(5, self.HFH)
+		self.assertEqual({}, self.portfolio.stocks)
+					
+	def test_createMutualFund(self):
+		self.assertEqual("BRT", self.BRT.tick_sym)
+		self.assertEqual("GHT", self.GHT.tick_sym)
+
+	def test_buyMutualFunds(self):
+		self.portfolio.addCash(300.50) 
+		self.portfolio.buyMutualFund(3, self.BRT)
+		self.assertEqual({"BRT" : 3}, self.portfolio.mutual_funds)
+		self.assertEqual(297.50, self.portfolio.funds) 
+		
+	def test_sellMutualFunds(self):
+		self.portfolio.addCash(300.50) 
+		self.portfolio.buyMutualFund(3, self.BRT)
+		self.portfolio.sellMutualFund(1.5, self.BRT)
+		self.assertEqual({"BRT" : 1.5}, self.portfolio.mutual_funds)
+	
+	def test_printPortfolio(self):
+		self.portfolio.addCash(300.50) 
+		self.portfolio.buyMutualFund(3, self.BRT)		
+		self.portfolio.buyStock(5, self.HFH)
+		self.assertEqual("Available Funds: $197.50 \nStocks: \n\t5  HFH\nMutual Funds: \n\t3  BRT\n", self.portfolio.__str__())
 
 if __name__ == '__main__':
   unittest.main() 
--- hw2/portfolio.py
+++ hw2/portfolio.py
@@ -4,23 +4,200 @@
 # August 10-11, 2014
 #########
 
+from random import uniform
+
+###########the main class for the homework
 class Portfolio():
 	
-	def __init__(self, account_holder):
-		self.funds = 0
-		self.stocks = {}
-		self.mutual_funds = {}
+#########required arguments are account holder (a name) and the initial deposit.  
+	def __init__(self, account_holder, initial_deposit = 0):
+		self.funds = initial_deposit
+		self.stocks = {} #store stocks purchased here
+		self.mutual_funds = {} #store mutual funds purchased here 
 		self.account_holder = account_holder 
+		self.history = AuditLog( #store account history here!
 	
+############for making a deposit to the account
 	def addCash(self, amount):
+		#try/except makes sure that you're giving the right input.  
 		try:
+			float(amount)
+			if amount <= 0:
+				raise Exception 
+		except:
+			print "Not a valid amount of money.  Please enter numeric characters greater than zero only."
+			raise Exception
+		self.funds += amount
+		self.history.append("$%.2f deposited.  Total balance now $%.2f." % (amount, self.funds))
+		print "Congrats! $%.2f added to your account.  Total balance now $%.2f." % (amount, self.funds)
+		
+	def withdrawCash(self, amount):
+		try: #try/except to make sure you're giving the right input
 			float(amount)
 		except:
 			print "Not a valid amount of money.  Please enter numeric characters only."
 			raise Exception
-		self.funds += amount
-# 		
-# portfolio = Portfolio("Dalston Ward")
-# portfolio.addCash(50)
-# print portfolio.funds
-# #portfolio.addCash("Fifty")
+		if self.funds >= amount: # Can't take out more money than you're got! 
+			self.funds -= amount
+			self.history.append("$%.2f withdrawn.  Total balance now $%.2f." % (amount, self.funds))
+			print "Congrats!  $%.2f withdrawn from your account.  Total balance now $%.2f." % (amount, self.funds)
+		else:
+			print "Withdraw amount exceeds account balance. Transaction not completed."	
+			
+######## function to buy some new stocks.  		
+	def buyStock(self, n_shares, stock_name):
+		try:
+			int(n_shares) #can only buy integer numbers of stocks
+			if n_shares <= 0:
+				raise Exception
+		except:
+			print "Can only buy positive, whole number shares of stocks.  Please enter a valid number of shares"
+			raise Exception
+		if n_shares * stock_name.price > self.funds:
+			print "Can't complete transaction.  Not enough funds!"
+		else:
+			self.funds -= n_shares*stock_name.price #update funds
+			if stock_name.tick_sym not in self.stocks.keys(): #create a key if not pre-existing
+				self.stocks[stock_name.tick_sym] = n_shares
+			else:
+				self.stocks[stock_name.tick_sym] += n_shares #else update key
+			print "Congrats!  You purchased %d share(s) of %s at a total cost of $%.2f." % (n_shares, stock_name.tick_sym, n_shares * stock_name.price)			
+		
+#########function to sell stocks back.  Takes as arguments the number of shares to sell and the stock to sell
+	def sellStock(self, n_shares, stock_name):
+		try: #make sure that its an integer number of shares and that you own that stock
+			int(n_shares)
+			if stock_name.tick_sym not in self.stocks.keys():
+				raise Exception
+		except:
+			print "Can only sell whole shares of stocks in your portfolio.  Please enter a valid number of shares a stock you own."
+			raise Exception		
+		if self.stocks[stock_name.tick_sym] < n_shares: #make sure that you aren't selling more shares than you own!
+			print "Cannot sell more shares than you own!  Transaction not completed."
+		else:		
+			if self.stocks[stock_name.tick_sym] == n_shares: #remove this stock from your portfolio entirely if selling all shares 
+				self.stocks.pop(stock_name.tick_sym, None)				
+			else: #just update your number of shares
+				self.stocks[stock_name.tick_sym] -= n_shares			
+			income = n_shares * uniform(.5 * stock_name.price, 1.5 * stock_name.price) #sale price
+			self.funds += income #update funds
+			print "Congrats!  You earned $%.2f from this sale" % income
+
+########## function to buy mutual funds.  Takes as arguments how much to buy and which fund to purchase
+	def buyMutualFund(self, shares, mf_name):
+		try: #make sure that its an appropriate number of shares to purchase
+			float(shares)
+			if shares <= 0:
+				raise Exception
+		except:
+			print "Can only buy positive shares of stocks.  Please enter a valid number of shares."
+			raise Exception	
+		if shares > self.funds: #make sure you can afford this transaction
+			print "Can't complete transaction.  Not enough funds!"
+		else:
+			self.funds -= shares #update funds
+			if mf_name.tick_sym not in self.mutual_funds.keys(): #create a key if not pre-existing
+				self.mutual_funds[mf_name.tick_sym] = shares
+			else:
+				self.mutual_funds[mf_name.tick_sym] += shares #else update key
+			print "Congrats!  You purchased %.f share(s) of %s at a total cost of $%.2f." % (shares, mf_name.tick_sym, shares)		
+
+#########function to sell mutual funds shares back.  Takes as arguments the number of shares to sell and the mutual fund to sell
+	def sellMutualFund(self, shares, mf_name):
+		try: #make sure that its a positive number and that you own that stock
+			float(shares)
+			if mf_name.tick_sym not in self.mutual_funds.keys():
+				raise Exception
+			if shares <= 0:
+				raise Exception
+		except:
+			print "Can only sell positive shares of mutual funds in your portfolio.  Please enter a valid number of shares a mutual fund you own."
+			raise Exception		
+		if self.mutual_funds[mf_name.tick_sym] < shares: #make sure that you aren't selling more shares than you own!
+			print "Cannot sell more shares than you own!  Transaction not completed."
+		else:	
+			if self.mutual_funds[mf_name.tick_sym] == shares: #remove this mutual fund from your portfolio entirely if selling all shares 
+				self.mutual_funds.pop(mf_name.tick_sym, None)				
+			else: #just update your number of shares
+				self.mutual_funds[mf_name.tick_sym] -= shares			
+			income = shares * uniform(0.9, 1.2) #sale price
+			self.funds += income #update funds
+			print "Congrats!  You earned $%.2f from this sale" % income	
+	
+######### function to print the portfolio
+	def __str__(self):
+		owner_print = "Account holder: %s" % self.account_holder
+		funds_print = "Available Funds: $%.2f \n" % self.funds
+		stocks_print = "Stocks: \n"
+		for stock in self.stocks:
+		 		stocks_print += ("\t" +  str(self.stocks[stock]) + "  " + stock + "\n")
+		mutual_funds_print = "Mutual Funds: \n"
+		for mutual_fund in self.mutual_funds:
+			mutual_funds_print += ("\t" +  str(self.mutual_funds[mutual_fund]) + "  " + mutual_fund + "\n")
+		return owner_print + funds_print + stocks_print + mutual_funds_print
+				
+
+		
+class Stock():
+	def __init__(self, price, tick_sym):
+		try:
+			if not isinstance(tick_sym, str): #Make sure the input is appropriate
+				raise Exception
+		except:
+			print "Not a valid ticker symbol.  Please enter a string."
+			raise Exception
+		try:
+			float(price)
+		except: 
+			print "Not a valid stock price.  Please enter numeric characters only" 
+			raise Exception
+		self.tick_sym = tick_sym
+		self.price = price
+		
+	def __str__(self):
+		return "Stock name: %s; Price per share: $%d" % (self.tick_sym, self.price)
+	
+class MutualFund():
+	def __init__(self, tick_sym):
+		try:
+			if not isinstance(tick_sym, str): #Make sure the input is appropriate
+				raise Exception
+		except:
+			print "Not a valid ticker symbol.  Please enter a string."
+			raise Exception
+		self.tick_sym = tick_sym
+	
+	def __str__(self):
+		return "Mutual Fund name is %s." % self.tick_sym
+	
+#		
+portfolio = Portfolio("Dalston Ward")
+portfolio.addCash(50)
+print portfolio.funds
+#portfolio.addCash("Fifty")
+#portfolio.addCash(-5)
+stock = Stock(22, "woo")
+stock2 = Stock(5, "DGW")
+portfolio.buyStock(1, stock)
+print portfolio.stocks
+print portfolio
+portfolio.addCash(50)
+print portfolio.funds
+portfolio.buyStock(1, stock)
+print portfolio.stocks
+portfolio.buyStock(3, stock2)
+print portfolio.stocks
+portfolio.sellStock(3, stock2)
+print portfolio.stocks
+portfolio.withdrawCash(2)
+print portfolio.history
+mf1 = MutualFund("HEY")
+mf2 = MutualFund("HO")
+portfolio.buyMutualFund(1.7,mf1)
+print portfolio.mutual_funds
+portfolio.sellMutualFund(.5, mf1)
+portfolio.buyMutualFund(.9, mf2)
+print portfolio.mutual_funds
+print portfolio
+portfolio.sellMutualFund(.5, mf1)
+print portfolio
