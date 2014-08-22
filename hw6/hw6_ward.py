9b3b92bdee68814d6781c11904fe2fa01e9f8b91
warddalston <ward.dalston@gmail.com>
2014-08-22 16:57:30 -0500

    Homework 6
    Dalston’s code for homework six.

--- hw6/hw6_ward.py
+++ hw6/hw6_ward.py
@@ -1,45 +1,88 @@
-#Register an app: https://dev.twitter.com/
+#Homework 6  - Dalston Ward
 
-#pip install tweepy
 import time
 import tweepy
+import csv 
 
-#First parameter is Consumer Key, second is Consumer Secret 
-auth = tweepy.OAuthHandler('your consumer key', '')
-auth.set_access_token('your consumer secret', '')    
-api = tweepy.API(auth)
+# #use this function to figure out which user is the most followed/active 
+def max_finder(input):
+	for user in input.keys():
+		if input[user] == max(input.values()):
+			return [user,input[user]]
 
-#See rate limit
-api.rate_limit_status()
+# #First parameter is Consumer Key, second is Consumer Secret 
+# auth = tweepy.OAuthHandler('', '')
+# auth.set_access_token('', '')    
+# api = tweepy.API(auth)
+# 
+# #See rate limit
+# api.rate_limit_status()
+# 
+# #Get some user
+# target = api.get_user('keithschnak')
 
-# #Get all tweets, https://dev.twitter.com/docs/api/1/get/statuses/public_timeline
-# public_tweets = api.public_timeline()
-# for t in public_tweets:
-#   #Note I am handling UTF encoded strings so I convert them to ASCII-compatible for my mac
-#   print "{0}: {1}".format(t.user.screen_name.encode('ascii', 'ignore'), t.text.encode('ascii', 'ignore'))
+#create CSV stuff for target's followers
 
-#Get some user
-target = api.get_user('kiethschnak')
+# #What info do we want? 
+# headers = ["user","N_followers"]
+# 
+# #Where do we save info?
+# filename = "followers.csv"
+# readFile = open(filename, "wb")
+# csvwriter = csv.writer(readFile)
+# csvwriter.writerow(headers)
+# 
+# 
 
-followers = []
-for page in tweepy.Cursor(api.followers, screen_name=target.screen_name).pages():
-    followers.extend(page)
+# #the .items bit says how many users to look at 
+# for user in tweepy.Cursor(api.followers, screen_name=target.screen_name).items():
+#      csvwriter.writerow([user.screen_name, user.followers_count])
+#      time.sleep(5)
+# 
+# readFile.close()
+
+################################
+#now scrape Keith's friends ####
+################################
+
+
+#create CSV stuff
+
+# #What info do we want? 
+# headers = ["user","N_tweets","N_favourites"]
+# 
+# #Where do we save info?
+# filename = "friends.csv"
+# readFile = open(filename, "wb")
+# csvwriter = csv.writer(readFile)
+# csvwriter.writerow(headers)
+# 
+# #do the scrape 
+# 
+# for user in tweepy.Cursor(api.friends, screen_name=target.screen_name).items():
+#       csvwriter.writerow([user.screen_name, user.statuses_count, user.favourites_count])
+#       time.sleep(5)
+# 
+# readFile.close()    
+
+#now do the calculations necessary
+
+Keith_followers = {}
+with open('followers.csv', 'rb') as f:
+  my_reader = csv.reader(f)
+  my_reader.next()
+  for row in my_reader:
+     Keith_followers[row[0]] = int(row[1])   
+       
+print max_finder(Keith_followers) #['thehill', 463770]
+
+Keith_friends = {}
+with open('friends.csv', 'rb') as f:
+  my_reader = csv.reader(f)
+  my_reader.next()
+  for row in my_reader:
+     Keith_friends[row[0]] = int(row[1]) + int(row[2]) 
     
-for f in followers:
-  #Note I am handling UTF encoded strings so I convert them to ASCII-compatible for my mac
-  print "{0}".format(f.screen_name.encode('ascii', 'ignore'))
+print max_finder(Keith_friends) #['HuffPostPol', 215474]
 
-
-#### prints everything 
-targets_followers = {}
-#the .items bit says how many users to look at 
-for user in tweepy.Cursor(api.followers, screen_name=target.screen_name).items():
-     targets_followers[user.screen_name] = user.followers_count
-     time.sleep(5)
-
-#prints the user's followers 
-for user in targets_followers.keys():
-	if targets_followers[user] == max(targets_followers.values()):
-		print user
-		
 		
